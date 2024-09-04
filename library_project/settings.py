ae51ffdf0cd1747db3557cdf7e006d92303a2033
Ayush Bhattarai <hello@ayushb.com>
2024-09-05 00:28:04 +0545

    Update settings.py

--- library_project/settings.py
+++ library_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q7im(aq=-uxr^lq#!$sxpflt1l=hu=86@^cid1v&z7m&=3c6ug"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", ".ayushb.com"]
 
